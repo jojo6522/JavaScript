17dab68ad4a87eab82e5680043cd22176b404dae
jojo6522 <61921257+jojo6522@users.noreply.github.com>
2020-12-22 10:47:49 -0500

    Create Ex1.js

---
+++ Chapter2/Ex1.js
@@ -0,0 +1,21 @@
+/* 
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/ 
+
+
+output = '';
+console.log(\n);
+
+for(i=1;i<=7;i++)
+{
+    output = output+ '#';
+    console.log(output);
+}
