4a1db59af136a228f8e380a27c2b9a52f16cd1f3
jojo6522 <61921257+jojo6522@users.noreply.github.com>
2021-01-04 09:13:05 -0500

    Create Ex1.js

---
+++ Chapter5/Ex1.js
@@ -0,0 +1,18 @@
+/*
+
+Flattening
+
+Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+ → [1, 2, 3, 4, 5, 6]
+
+*/
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+function join(a,b){
+    return a.concat(b);
+}
+
+let answer = arrays.reduce(join);
+console.log(answer);
